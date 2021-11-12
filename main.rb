cdf3c03711a18f7163775e573a6061a4a16c59e5
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-12 12:07:37 -0600

    add basic UI, fix bugs

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
 # test.rb
-require 'pry'
-require 'classroom'
-require 'person'
-require 'rental'
-require 'student'
-require 'teacher'
-require 'book'
+require './classroom'
+require './person'
+require './rental'
+require './student'
+require './teacher'
+require './book'
 # rubocop:disable all
 # Create class App
 class App
-  binding.pry
   def initialize
     @books = []
     @people = []
@@ -151,3 +149,4 @@ def main
     puts "\n"
   end
 end
+main()
